08a947cc859f7939f785890238ec20a227c5bd94
Yutang Lin <yutanglin16@gmail.com>
2018-06-05 04:35:41 -0700

    Function files

---
+++ SoftServerless/RatingDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftServerless
+{
+    class RatingDto
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid ProductId { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string LocationName { get; set; }
+        public int Rating { get; set; }
+        public string UserNotes { get; set; }
+
+    }
+}
---
+++ SoftServerless/Ratings.cs
@@ -0,0 +1,47 @@
+
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
+using System;
+using Newtonsoft.Json.Serialization;
+
+namespace SoftServerless
+{
+    public static class Ratings
+    {
+        [FunctionName("CreateRating")]
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]HttpRequest req, TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+            
+            string requestBody = new StreamReader(req.Body).ReadToEnd();
+            var data = JsonConvert.DeserializeObject<RatingDto>(requestBody);
+
+            if (!Validate(data))
+            {
+                return new BadRequestObjectResult("Request not valid");
+            }
+
+            data.Id = Guid.NewGuid();
+            data.Timestamp = DateTime.UtcNow;
+
+            return new JsonResult(data, new JsonSerializerSettings
+            {
+                ContractResolver = new DefaultContractResolver()
+                {
+                    NamingStrategy = new CamelCaseNamingStrategy()
+                },
+                Formatting = Formatting.Indented,
+            });
+        }
+
+        private static bool Validate(RatingDto data)
+        {
+            return true;
+        }
+    }
+}
