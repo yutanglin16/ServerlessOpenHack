8296265b24d15135e9ee8f52bb1a584635425c9d
Omeed Musavi <omusavi@microsoft.com>
2018-06-06 08:35:21 -0700

    Adding Sales function

---
+++ SoftServerless/Sales.cs
@@ -0,0 +1,26 @@
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.ServiceBus;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftServerless
+{
+    public static class Sales
+    {
+        [FunctionName("Sales")]
+        public static async Task Run([EventHubTrigger("sales", Connection = "SalesEventHub")]string[] myEventHubMessages, 
+            [CosmosDB(
+                databaseName: "SoftServerless",
+                collectionName: "Sales",
+                ConnectionStringSetting = "RatingsDb")] IAsyncCollector<string> docs, 
+            TraceWriter log)
+        {
+            foreach (var message in myEventHubMessages)
+            {
+                await docs.AddAsync(message);
+            }
+        }
+    }
+}
+    
